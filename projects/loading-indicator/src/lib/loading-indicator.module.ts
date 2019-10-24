8b0b72abc1961cf4949ec3d56c10fb8a27caf8ea
Adam Molnar <molnar.adam@e-vista.hu>
2019-10-24 12:33:01 +0200

    Init

---
+++ projects/loading-indicator/src/lib/loading-indicator.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LoadingIndicatorComponent } from './loading-indicator.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [LoadingIndicatorComponent],
+  exports: [LoadingIndicatorComponent]
+})
+export class LoadingIndicatorModule { }
